4e3a5952fc6c748b3fe4ac648c82968a18d3f39f
jo <jo@pop-os.fritz.box>
2026-08-01 12:57:37 +0200

    feat: add inactive capability catalogue

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -8,3 +8,4 @@ export * from "./policy-evaluator.js";
 export * from "./paperless-zoo-adapter.js";
 export * from "./effective-rights.js";
 export * from "./injection-trust-boundary.js";
+export * from "./capability-catalogue.js";
